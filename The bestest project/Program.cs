637535665fc68e97a8f0c54f58c0c213fdd6b284
Happy <kolesnikjenya8@gmail.com>
2023-10-13 21:21:11 +0300

    My first comment

---
+++ The bestest project/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] A = new int[20];
+int[] B = new int[20];
+Random random = new Random();
+
+for (int i = 0; i < A.Length; i++)
+{
+    A[i] = random.Next(888);
+}
+for (int i = 0; i < B.Length; i++)
+{
+    B[i] = random.Next(100);
+}
+
+Array.Sort(B);
+Array.Reverse(B);
+
+foreach (int i in A)
+{
+    Console.WriteLine(i);
+}
+foreach (int j in B)
+{
+    Console.WriteLine(j);
+}
