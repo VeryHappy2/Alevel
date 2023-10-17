34e56f6918999600d3c03afaac5c99dc7fc30fc5
Happy <kolesnikjenya8@gmail.com>
2023-10-17 23:46:41 +0300

    It's my projects

---
+++ My project2/Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+while (true)
+{
+    Console.WriteLine("Please write first number, + or / or * and second number");
+
+    if (!double.TryParse(Console.ReadLine(), out double number1) ||
+    !char.TryParse(Console.ReadLine(), out char a) ||
+    !double.TryParse(Console.ReadLine(), out  double number2))
+    {
+        Console.WriteLine("Please write valid numbers.");
+        break;
+    }
+    double result = 0;
+
+    switch (a)
+    {
+        case '+':
+            result = number1 + number2;
+            break;
+        case '-':
+            result = number1 - number2;
+            break;
+        case '*':
+            result = number1 * number2;
+            break;
+        case '/':
+            if (number1 == 0 || number2 == 0)
+            {
+                Console.WriteLine("Error: number is 0");
+                break;
+
+            }
+            result = number1 / number2;
+            break;
+        default:
+            Console.WriteLine("Please use *,/,+,-");
+            break;
+
+    }
+    Console.WriteLine($"Result: {result:F2}");
+    Console.WriteLine("Would you want continue?");
+    string answer = Console.ReadLine();
+    
+    if (answer == "no")
+    {
+        break;
+    }
+    else if ( answer == "yes" ) 
+    {
+        continue;
+    }
+}
+
---
+++ My project2/My project/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+var a = false;
+string[] newarray = new string[10];
+void Note()
+{
+    
+    Console.WriteLine("Please write  note");
+
+    string note = Console.ReadLine();
+    newarray[0] = note;
+    Console.WriteLine("Do you want see note?");
+    string user = Console.ReadLine();
+
+    if (user.ToLower() == "no" )
+    {
+        Console.Clear(); 
+    }
+    else if (user.ToLower() == "yes" || user.ToLower() == "yeah") 
+    {
+        
+    }
+}
+
+
+while (a == false)
+{
+    Note();
+}
+
+
+
+
+
