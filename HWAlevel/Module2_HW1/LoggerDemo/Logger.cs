c81694a319efb80e742fc0818292d2122b4b7eb6
Happy <kolesnikjenya8@gmail.com>
2023-10-28 14:07:08 +0300

    Module2_HW1 added

---
+++ HWAlevel/Module2_HW1/LoggerDemo/Logger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerDemo
+{
+    internal class Logger
+    {
+        readonly public string logPath = ConfigurationManager.AppSettings["logPath"];
+
+        public  void LogWarning(string message)
+        {
+            using (StreamWriter writerWarning = new StreamWriter(logPath, true))
+            {
+                writerWarning.WriteLine($"{DateTime.Now} : [Warning] {message}");
+            }
+
+        }
+
+        public  void LogError(string message)
+        {
+            using (StreamWriter writerError = new StreamWriter(logPath, true))
+            {
+                writerError.WriteLine($"{DateTime.Now} : [Error] {message}");
+            }
+        }
+
+        public  void LogInfo(string message)
+        {
+            using (StreamWriter writerInfo = new StreamWriter(logPath, true))
+            {
+             writerInfo.WriteLine($"{DateTime.Now} : Info");
+            }
+        }
+
+        //public static void WriteLogger(string message)
+        //{
+        //string logPath = ConfigurationManager.AppSettings["logPath"];
+
+        //using (StreamWriter writer = new StreamWriter(logPath, true))
+        //{
+        // writer.WriteLine($"{DateTime.Now} : {message}");
+        //}
+    }
+}
---
+++ HWAlevel/Module2_HW1/LoggerDemo/Starter.cs
@@ -0,0 +1,42 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerDemo
+{
+    internal class Starter
+    {
+        public static void Run()
+        {
+            Action action = new Action();
+
+            Random random = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int index = random.Next(3);
+
+                if (index == 0)
+                {
+                    action.ResultInfo("Method1");
+                }
+                else if (index == 1) 
+                {
+                    action.ResultWarning("Method2");
+                }
+                else
+                {
+                    action.ResultError("I broke a logic in Method3");
+                    
+                }
+
+            }
+
+
+        }
+
+    }
+}
