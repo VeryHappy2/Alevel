cd185d1f93362ef929f1d52181e4bdf6427c666e
Happy <kolesnikjenya8@gmail.com>
2023-11-04 10:59:17 +0200

    Module 2 HW 3 added

---
+++ HWAlevel/Module2_HW3/Candies/Program/Program.cs
@@ -0,0 +1,117 @@
+﻿using Module2_HW3.Candies.Wafer_candies;
+using Module2_HW3.Candies.Сherry_candies;
+using Module2_HW3.Candies.Сhoclats_candies;
+
+namespace Module2_HW3.Candies.Program
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            try
+            {
+                Console.WriteLine("How did candies bought?");
+
+                List<Candies> basket = new List<Candies>();
+                List<Candies> takeAway = new List<Candies>();
+
+                while (true)
+                {
+                    Console.Write("'1' Start: \n'2' Take candies: \n'3' To exit:\n ");
+                    string answer = Console.ReadLine();                    
+
+                    if (answer == "1")
+                    {
+
+
+                        Console.WriteLine($"Please put as a gift сandies (\"KitKat\", \"Snickers\", \"Kirsh\", \"CherryFountain\", \"FerreroRocher\", \"LintLindor\")");
+                        string putGift = Console.ReadLine();
+
+                        Candies candy = CreateCandyByName(putGift);
+
+                        basket.Add(candy);
+                        Console.WriteLine("Gift:");
+                        foreach (Candies candy1 in basket)
+                        {
+                            Console.WriteLine($"{candy1.Name}.It's has {candy1.resultgram} gram");
+                        }
+
+                        if (candy == null)
+                        {
+                            Console.WriteLine("Invalid candy name. Please try again.");
+                        }
+
+                    }
+
+                    if (answer == "3")
+                    {
+                        break;
+                    }
+
+                    if (answer == "2")
+                    {
+                        try
+                        {
+                            foreach (Candies candy1 in basket)
+                            {
+                                Console.WriteLine($"Will take candies from a gift {candy1.Name} ");
+                            }
+
+                            string candiesFromGift = Console.ReadLine();
+
+                            Candies candyToRemove = basket.FirstOrDefault(c => c.Name == candiesFromGift);
+
+                            if (candyToRemove != null)
+                            {
+
+                                basket.Remove(candyToRemove);
+                                Console.WriteLine($"You took from a gift: {candyToRemove.Name}");
+                                takeAway.Add(candyToRemove);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid candy name. Please try again.");
+                            }
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+
+                    }
+
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+        public static Candies CreateCandyByName(string name)
+        {
+            switch (name)
+            {
+                case "KitKat":
+                    return new KitKat();
+                case "Snickers":
+                    return new Snickers();
+                case "Kirsh":
+                    return new Kirsh();
+                case "CherryFountain":
+                    return new CherryFountain();
+                case "FerreroRocher":
+                    return new FerreroRocher();
+                case "LintLindor":
+                    return new LintLindor();
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ HWAlevel/Module2_HW3/Candies/Сherry candies/CherryCandies.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace Module2_HW3.Candies.Сherry_candies
+{
+    public class CherryCandies : Candies
+    {
+        public int cherryCandiesGram = 20;
+    }
+    public class Kirsh : CherryCandies
+    {
+        public Kirsh() 
+        {
+            Name += "Kirsh";
+            resultgram += cherryCandiesGram;
+        }
+    }
+    public class CherryFountain : CherryCandies
+    {
+        public CherryFountain()
+        {
+            Name += "CherryFountain";
+            resultgram += cherryCandiesGram;
+        }
+    }
+}
