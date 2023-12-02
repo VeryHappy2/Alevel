9ec2e7c4f60d0980189059139837049af4488b10
Happy <kolesnikjenya8@gmail.com>
2023-12-02 15:21:38 +0200

    HW5 was added

---
+++ HWAlevel/Moudule3/Module3_HW5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Module3_HW5
+{
+    internal sealed class Program
+    {
+        static async Task Main()
+        {
+            Read read = new Read();
+            await read.MethodRead();
+            Console.WriteLine($"{read.hello.Trim()} {read.world.Trim()}" );
+        }
+    }
+}
