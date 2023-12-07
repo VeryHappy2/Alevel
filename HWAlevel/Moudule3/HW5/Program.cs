8eda1609076431ec63bb619c39aebb308a185660
Happy <kolesnikjenya8@gmail.com>
2023-12-07 20:59:10 +0200

    Module 3, HW 5 was added

---
+++ HWAlevel/Moudule3/HW5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HW5
+{
+    internal sealed class Program
+    {
+        private delegate void MyDelegate (State newState);
+        private event MyDelegate myEvent;
+
+        private State ranState { get; set; }
+        static async Task Main()
+        {            
+            var program = new Program();
+            await program.Open();
+        }
+        public async Task Open()
+        {
+            myEvent += MyMethod;
+            var source = new CancellationTokenSource();
+            CancellationToken cancellation = source.Token;
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                source.Cancel();
+            };
+
+            cancellation.Register(
+                () => Console.WriteLine("window was closed by the user"));         
+
+            while (!cancellation.IsCancellationRequested) 
+            {
+                myEvent?.Invoke(ranState);
+                Console.WriteLine($"Event: {ranState}");
+                Console.WriteLine("window is open");
+                await Task.Delay(3000);
+            }
+
+
+        }
+
+        public void MyMethod(State newState)
+        {
+            Random random = new Random();
+            var values = Enum.GetValues(typeof(State));
+            ranState = (State)values.GetValue(random.Next(values.Length));
+
+        }
+
+    }
+}
