870c0115397c44e65d93696e177dfc4f3322a462
Happy <kolesnikjenya8@gmail.com>
2023-12-31 11:45:19 +0200

    Module 2 was completed

---
+++ HWAlevel/Module2/Module2_HW/StartProgram/AppStart.cs
@@ -0,0 +1,47 @@
+﻿using Module2_HW.CustomExeptions;
+using Module2_HW.MainLogic;
+using System;
+using Action = Module2_HW.MainLogic.Action;
+
+namespace Module2_HW.StartProgram
+{
+
+    public class App
+    {
+        public static void Run()
+        {
+            Action actions = new Action();
+            Random random = new Random();
+
+            for (int counter = 0; counter < 100; counter++)
+            {
+                int randomAction = random.Next(1, 4);
+
+                try
+                {
+                    switch (randomAction)
+                    {
+                        case 1:
+                            actions.StartMethod();
+                            break;
+                        case 2:
+                            actions.SkippedLogic();
+                            break;
+                        case 3:
+                            actions.BrokeLogic();
+                            break;
+                    }
+                }
+                catch (BusinessException ex)
+                {
+                    Logger.Log("Warning", $"Action got this custom Exception: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log("Error", $"Action failed by reason: {ex.Message}");
+                }
+            }
+        }
+
+    }
+}
