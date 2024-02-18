f06eec8c933fd421e8f1fbab8c0158039bf23a87
Happy <kolesnikjenya8@gmail.com>
2024-02-18 19:33:43 +0200

    Module6_HW1 was added

---
+++ HWAlevel/Module6/Module6_HW1/Products.cs
@@ -0,0 +1,11 @@
+namespace Module6_HW1
+{
+    public class Products
+    {
+        public DateOnly ReleaseDate { get; set; }
+
+        public int Price { get; set; }
+
+        public string? Product { get; set; }
+    }
+}
